843429346a43ada685c94b36484a092c02667066
Luis Adiel Martínez <luisadiel99@gmail.com>
2023-12-15 20:55:29 -0600

    Add common properties

--- ResEvi/App.xaml.cs
+++ ResEvi/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 
 namespace ResEvi
 {
@@ -7,5 +9,24 @@ namespace ResEvi
     /// </summary>
     public partial class App : Application
     {
+        private static string? _localDataDirectory = null;
+
+        public static string AppName => "ResEvi";
+
+        public static string DatabaseFilePath => Path.Combine(LocalDataDirectory, "database.db");
+
+        public static string LocalDataDirectory
+        {
+            get
+            {
+                if (_localDataDirectory == null)
+                {
+                    string dir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                    _localDataDirectory = Path.Combine(dir, AppName);
+                }
+
+                return _localDataDirectory;
+            }
+        }
     }
 }
