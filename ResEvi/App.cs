79a0826fe7a16608a2934bf90fd75546f0878311
Luis Adiel Martínez <luisadiel99@gmail.com>
2024-02-15 22:59:32 -0600

    Port project to WinForms

---
+++ ResEvi/App.cs
@@ -0,0 +1,46 @@
+﻿namespace ResEvi
+{
+    internal sealed class App
+    {
+        private static string? m_dataDirectory = null;
+
+        /// <summary>
+        /// Gets the full path of the application's data directory.
+        /// </summary>
+        public static string DataDirectory
+        {
+            get
+            {
+                if (m_dataDirectory == null)
+                {
+                    string directory = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                    m_dataDirectory = Path.Combine(directory, "ITCM");
+                }
+
+                return m_dataDirectory;
+            }
+        }
+
+        /// <summary>
+        /// Gets the full path of the archive directory root.
+        /// </summary>
+        public static string ArchiveDirectory
+        {
+            get
+            {
+                return Path.Combine(DataDirectory, "Archive");
+            }
+        }
+
+        /// <summary>
+        /// Gets the full path of the main SQLite database file.
+        /// </summary>
+        public static string DatabaseFile
+        {
+            get
+            {
+                return Path.Combine(DataDirectory, "database.db");
+            }
+        }
+    }
+}
---
+++ ResEvi/MainForm.cs
@@ -0,0 +1,10 @@
+namespace ResEvi
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
